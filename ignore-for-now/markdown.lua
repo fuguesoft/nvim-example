f7e9b356b5cdf63779600f54c0628da01fb5733b
fuguesoft <31870368+fuguesoft@users.noreply.github.com>
2026-04-22 22:17:21 -0500

    Remove ignored directory

--- ignore-for-now/markdown.lua
+++
@@ -1,9 +0,0 @@
-local set = vim.opt_local
-
-set.wrap = true -- wrap text
-set.breakindent = true -- Match indent on line break
-set.linebreak = true -- Line break on whole words
-
--- Allow j/k when navigating wrapped lines
-vim.keymap.set('n', 'j', 'gj')
-vim.keymap.set('n', 'k', 'gk')
--- ignore-for-now/matchy.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd('BufEnter', {
-  group = vim.api.nvim_create_augroup('MatchyMatchy', { clear = true }),
-  pattern = '*',
-  callback = function()
-    vim.cmd [[ set matchpairs+=<:> ]]
-  end,
-})
-return {}
